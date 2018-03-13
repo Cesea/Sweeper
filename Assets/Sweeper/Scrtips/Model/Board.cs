9f5fd40a268d42820e81115d17015352de6d591f
chi-su <cltnmm@naver.com>
2018-03-13 11:01:53 +0900

    prefab change

--- Assets/Sweeper/Scrtips/Model/Board.cs
+++ Assets/Sweeper/Scrtips/Model/Board.cs
@@ -84,12 +84,12 @@ public class Board
 
         Nodes[Index3D(0, 1, 1)].Type = Node.NodeType.Normal;
         Nodes[Index3D(0, 1, 2)].Type = Node.NodeType.Normal;
-        Nodes[Index3D(0, 2, 2)].Type = Node.NodeType.Normal;
+        //Nodes[Index3D(0, 2, 2)].Type = Node.NodeType.Normal;
 
 
         Nodes[Index3D(6, 1, 4)].Type = Node.NodeType.Normal;
         Nodes[Index3D(6, 1, 5)].Type = Node.NodeType.Normal;
-        Nodes[Index3D(6, 2, 5)].Type = Node.NodeType.Normal;
+        //Nodes[Index3D(6, 2, 5)].Type = Node.NodeType.Normal;
         Nodes[Index3D(6, 1, 6)].Type = Node.NodeType.Normal;
 
         StartCellCoord = new Vector3Int(0, 0, 0);
